414db1bd7b74fc595aad1730fee7ae96743cc57b
Andrew Choe <andychoe@wireless-10-104-178-88.umd.edu>
2022-11-16 04:26:34 -0500

    day 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,10 @@ import Banner from '../components/Banner'
 import requests from '../utils/requests'
 import { Movie } from '../typings'
 import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
+import { useRecoilValue } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
+import Modal from '../components/Modal'
 
 interface Props{
   netflixOriginals: Movie[]
@@ -26,17 +30,17 @@ const Home = ({
   topRated, 
   trendingNow,
 }: Props) => {
+  const { loading } = useAuth()
+  const showModal = useRecoilValue(modalState)
+
+  if(loading) return null
+
   return (
-    <div className="relative h-screen bg-grad-to-b from-gray-900/10
-    to-[#010511] lg:h-[140vh]">
-      <Head>
-        <title>Home - Netflix</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="relative h-screen bg-grad-to-b lg:h-[140vh]">
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
+        <section className="md:space-y-24">
         <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
@@ -47,7 +51,7 @@ const Home = ({
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {/* Modal */}
+      {showModal && <Modal />}
     </div>
   )
 }
